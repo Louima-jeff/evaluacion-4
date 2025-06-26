54b19e61ba0f7659638677f108dc9574b32e851f
Louima-jeff <GEFMYLOUIMA1986@GMAIL.COM>
2025-06-25 21:59:29 -0400

    Add files via upload

---
+++ exmamen.py
@@ -0,0 +1,51 @@
+def cats():
+    print("compra entra a cats:" )
+    print("cambio de funcion:")
+    print("muestra stock de funciones:") 
+    print("salir:")
+    opcion=0
+    while True:
+       try:
+        opcion=int(input("ingrese opcion"))
+        
+        print("la opcion ingresada debe ser un numero:" )
+        exceptvaluesError:
+       else:
+    if opcion==1:
+       nombre=input("ingrese nombre del comprador:" )
+       
+       if nombre in compra_entrada:
+          print("el nombre ya existe:")
+
+          
+        
+          
+          
+        
+    
+          
+
+   
+
+
+
+       
+       
+
+
+       
+
+
+        
+
+        
+    
+            
+        
+
+    
+    
+
+    
+    
+    
